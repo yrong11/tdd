b557aae2babf903cf9b0783a09f828e693364a86
yurong <rong.yu@thoughtworks.com>
2020-08-03 12:13:45 +0800

    add test

---
+++ src/test/java/Mommifier.java
@@ -0,0 +1,9 @@
+
+
+public class Mommifier {
+
+    public String insert_mommy_new_string(String str){
+        return null;
+    }
+
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import java.lang.management.MonitorInfo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MommifierTest {
+
+    @Test
+    void given_string_when_vowels_more_than_30of_string_length_then_return_insert_mommy_new_string(){
+        Mommifier mommifier = new Mommifier();
+        String str = "is aeiou";
+        assertEquals("is amommyemommyimommyomommyu", mommifier.insert_mommy_new_string(str));
+    }
+
+    @Test
+    void given_string_when_vowels_less_than_30of_string_length_then_return_string(){
+        Mommifier mommifier = new Mommifier();
+        String str = "Hello World!";
+        assertEquals(str, mommifier.insert_mommy_new_string(str));
+    }
 }
