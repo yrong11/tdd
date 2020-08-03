a5f38c43196e8a4b592eb0f9e9f2cac4670cdcf0
yurong <rong.yu@thoughtworks.com>
2020-08-03 16:40:17 +0800

    complete the Mommifier

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,9 +1,43 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Mommifier {
+    ArrayList<Character> VOWELS = new ArrayList<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
-    public String insert_mommy_new_string(String str){
-        return null;
+    public String insert_mommy_for_string(String str){
+        int len = str.length();
+        int vowel_counts = get_string_vowels_counts(str);
+        float precent = 0.3f;
+        String insert_words = "mommy";
+
+        if (vowel_counts <= len * precent)
+            return str;
+
+        StringBuilder result = new StringBuilder();
+        boolean is_vowel_flag = is_vowels(str.charAt(0));
+        result.append(str.charAt(0));
+
+        for (int i = 1; i < len; i++){
+            if (is_vowel_flag && is_vowels(str.charAt(i)))
+                result.append(insert_words);
+            result.append(str.charAt(i));
+            is_vowel_flag = is_vowels(str.charAt(i));
+        }
+        return result.toString();
     }
+
+    public int get_string_vowels_counts(String str){
+        return (int) str.chars().mapToObj(c -> (char)c)
+                                .map(Character::toLowerCase)
+                                .filter(c -> VOWELS.contains(c))
+                                .count();
+    }
+
+    public boolean is_vowels(char c){
+        return VOWELS.contains(Character.toLowerCase(c));
+    }
+
+
 
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -12,21 +12,27 @@ public class MommifierTest {
     void should_insert_mommy_when_vowels_more_than_30_and_have_continue_vowels(){
         String testStr = "is aeiou";
         String testResult = "is amommyemommyimommyomommyu";
-        assertEquals(testResult, mommifier.insert_mommy_new_string(testStr));
+        assertEquals(testResult, mommifier.insert_mommy_for_string(testStr));
     }
 
     @Test
     void not_insert_mommy_when_vowels_more_than_30_but_not_have_continue_vowels(){
         String testStr = "is ahbo";
-        assertEquals(testStr, mommifier.insert_mommy_new_string(testStr));
+        assertEquals(testStr, mommifier.insert_mommy_for_string(testStr));
     }
 
     @Test
     void should_return_original_string_when_vowels_less_than_30_present(){
         String testStr1 = "Hello World!";
-        assertEquals(testStr1, mommifier.insert_mommy_new_string(testStr1));
+        assertEquals(testStr1, mommifier.insert_mommy_for_string(testStr1));
         String testStr2 = "aeio bnmfkzlpmqwmm";
-        assertEquals(testStr2, mommifier.insert_mommy_new_string(testStr2));
+        assertEquals(testStr2, mommifier.insert_mommy_for_string(testStr2));
+    }
+
+    @Test
+    void should_return_empty_string_when_input_empty_string(){
+        String testStr = "";
+        assertEquals(testStr, mommifier.insert_mommy_for_string(testStr));
     }
 
 }
