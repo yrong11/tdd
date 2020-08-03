0e3e2cce43d57d69618d7f0704b2c182d828043a
yurong <rong.yu@thoughtworks.com>
2020-08-03 15:47:41 +0800

    compelet test_case

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -6,17 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MommifierTest {
 
+    Mommifier mommifier = new Mommifier();
+
     @Test
-    void given_string_when_vowels_more_than_30of_string_length_then_return_insert_mommy_new_string(){
-        Mommifier mommifier = new Mommifier();
-        String str = "is aeiou";
-        assertEquals("is amommyemommyimommyomommyu", mommifier.insert_mommy_new_string(str));
+    void should_insert_mommy_when_vowels_more_than_30_and_have_continue_vowels(){
+        String testStr = "is aeiou";
+        String testResult = "is amommyemommyimommyomommyu";
+        assertEquals(testResult, mommifier.insert_mommy_new_string(testStr));
     }
 
     @Test
-    void given_string_when_vowels_less_than_30of_string_length_then_return_string(){
-        Mommifier mommifier = new Mommifier();
-        String str = "Hello World!";
-        assertEquals(str, mommifier.insert_mommy_new_string(str));
+    void not_insert_mommy_when_vowels_more_than_30_but_not_have_continue_vowels(){
+        String testStr = "is ahbo";
+        assertEquals(testStr, mommifier.insert_mommy_new_string(testStr));
     }
+
+    @Test
+    void should_return_original_string_when_vowels_less_than_30_present(){
+        String testStr1 = "Hello World!";
+        assertEquals(testStr1, mommifier.insert_mommy_new_string(testStr1));
+        String testStr2 = "aeio bnmfkzlpmqwmm";
+        assertEquals(testStr2, mommifier.insert_mommy_new_string(testStr2));
+    }
+
 }
